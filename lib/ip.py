2006b7e97fe0aea5e290928c221b59f3a9eda105
chanwoo kwon <arknell@yonsei.ac.kr>
2020-09-10 15:36:45 +0900

    ip address location check

--- lib/ip.py
+++ lib/ip.py
@@ -1,11 +1,14 @@
-from ip2geotools.databases.noncommercial import DbIpCity
-
+import json
+import requests
 
 class IPLocation:
     @staticmethod
     def get_region(ip):
         if ip == "127.0.0.1":
             return "local"
-        response = DbIpCity.get(ip, api_key='free')
 
-        return "{}, {}, {}".format(response.city, response.region, response.country)
+        URL = 'https://ipinfo.io/json'
+        response = requests.get(URL)
+        location = json.loads(response.text)
+
+        return "{}, {}, {}".format(location.org, location.city, location.country)
